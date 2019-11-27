a006d4608e54092b62fbb06d669482dab147114d
BrandenBent <bent_branden@hotmail.com>
2019-11-26 23:07:26 -0600

    initial commit

---
+++ src/com/tutorial/main/Player.java
@@ -0,0 +1,28 @@
+package com.tutorial.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player extends GameObject {
+
+	public Player(int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	@Override
+	public void tick() {
+		x += velX;
+		y += velY;
+		
+		x = Game.clamp(x, 0, Game.WIDTH - 30);
+		y = Game.clamp(y, 0, Game.HEIGHT - 54);
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, y, 32, 32);
+	}
+
+}
